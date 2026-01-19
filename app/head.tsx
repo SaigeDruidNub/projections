b488db72602bf1b1c3fa9c880e55571c97bd1745
Charity Zielony <charity@nubbernaut.com>
2026-01-19 10:02:36 -0500

    add nub favicon

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Projections - Project Management & Approvals</title>
+      <meta
+        name="description"
+        content="Manage projects, projections, checkpoints, and feature approvals"
+      />
+      <link rel="icon" href="/favicon.png" type="image/png" />
+    </>
+  );
+}
